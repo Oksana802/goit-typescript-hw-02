ad0e1162c99d8ff25449537706af774aedbf4401
Oksana <oksanamp2014@gmail.com>
2025-01-03 17:14:51 +0200

    fix

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,13 +3,18 @@ import { FetchRes, Img } from "../components/App/App.types";
 
 axios.defaults.baseURL = "https://api.unsplash.com";
 
+interface ApiFetchRes {
+  results: Img[];
+  total: number;
+}
+
 export const fetchImages = async (
   query: string,
   page: number,
   perPage: number = 12
 ): Promise<FetchRes> => {
   try {
-    const response = await axios.get(`/search/photos`, {
+    const response = await axios.get<ApiFetchRes>(`/search/photos`, {
       params: {
         client_id: "Opo1OXeY11YPl_Vh5V07W2pdWLleW6w5iS2PMl8kQyE",
         query,
@@ -22,24 +27,7 @@ export const fetchImages = async (
     const totalPages = Math.ceil(total / perPage);
 
     return {
-      results: results.map(
-        (image: any): Img => ({
-          id: image.id,
-          url: image.urls.small,
-          title: image.alt_description || "Untitled",
-          alt_description: image.alt_description || null,
-          description: image.description || null,
-          likes: image.likes || 0,
-          urls: {
-            small: image.urls.small,
-            regular: image.urls.regular,
-          },
-          user: {
-            name: image.user.name,
-            location: image.user.location || null,
-          },
-        })
-      ),
+      results,
       totalPages,
     };
   } catch (error) {
