485bc00d04cdceb5ed8488c93e372a7b4bfeeb94
Oksana <oksanamp2014@gmail.com>
2025-01-03 15:47:16 +0200

    ts-hw-2-build

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
+import { FetchRes, Img } from "../components/App/App.types";
 
 axios.defaults.baseURL = "https://api.unsplash.com";
 
-export const fetchImages = async (query, page, perPage = 12) => {
+export const fetchImages = async (
+  query: string,
+  page: number,
+  perPage: number = 12
+): Promise<FetchRes> => {
   try {
     const response = await axios.get(`/search/photos`, {
       params: {
@@ -16,9 +21,29 @@ export const fetchImages = async (query, page, perPage = 12) => {
     const { results, total } = response.data;
     const totalPages = Math.ceil(total / perPage);
 
-    return { results, totalPages };
+    return {
+      results: results.map(
+        (image: any): Img => ({
+          id: image.id,
+          url: image.urls.small,
+          title: image.alt_description || "Untitled",
+          alt_description: image.alt_description || null,
+          description: image.description || null,
+          likes: image.likes || 0,
+          urls: {
+            small: image.urls.small,
+            regular: image.urls.regular,
+          },
+          user: {
+            name: image.user.name,
+            location: image.user.location || null,
+          },
+        })
+      ),
+      totalPages,
+    };
   } catch (error) {
-    console.error("Error fetching images:", error.message);
+    console.error("Error fetching images:", (error as Error).message);
     throw error;
   }
 };
